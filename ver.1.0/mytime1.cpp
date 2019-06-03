91eea0b699a2ae9a67b6e3af456c2b4d045adbce
Deusia <46041166+duesia@users.noreply.github.com>
2019-06-03 11:00:46 +0800

    Add files via upload

--- ver.1.0/mytime1.cpp
+++ ver.1.0/mytime1.cpp
@@ -5,7 +5,7 @@ Time::Time()
 	hours=minutes=0;
 }
 
-Time::Time(int h,int m=0)
+Time::Time(int h,int m)
 {
 	hours=h;
 	minutes=m;
@@ -23,7 +23,7 @@ void Time::addHr(int h)
 	hours+=h;
 }
 
-void Time::Reset(int h=0,int m=0)
+void Time::Reset(int h,int m)
 {
 	hours=h;
 	minutes=m;
@@ -39,7 +39,39 @@ Time Time::sum(const Time & t)const
 	return sum;
 }
 
+Time Time::operator+(const Time & t) const
+{
+	Time sum;
+	sum.minutes=minutes+t.minutes;
+	sum.hours=hours+t.hours+sum.minutes/60;
+	sum.minutes%=60;
+
+	return sum;
+}
+
+Time Time::operator-(const Time & t) const
+{
+	Time sum;
+	int temp1;
+	int temp2;
+	
+	temp1=minutes+60*hours;
+	temp2=t.minutes+60*t.hours;
+	if(temp1>temp2)
+	{
+		sum.hours=(temp1-temp2)/60;
+		sum.minutes=(temp1-temp2)%60;
+	}
+	else
+	{
+		sum.hours=(temp2-temp1)/60;
+		sum.minutes=(temp2-temp1)%60;
+	}
+
+	return sum;
+}
+
 void Time::Show() const
 {
-	std::cout<<hours<<" hours,"<<minutes<<" minutes.";
+	std::cout<<hours<<" hours,"<<minutes<<" minutes."<<std::endl;
 }
