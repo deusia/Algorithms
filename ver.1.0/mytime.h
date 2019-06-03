9956eab2869bd40ce391a640efdff647c2cc26ed
Deusia <46041166+duesia@users.noreply.github.com>
2019-06-03 10:14:21 +0800

    Add files via upload

---
+++ ver.1.0/mytime.h
@@ -0,0 +1,17 @@
+#ifndef MYTIME_H_
+#define MYTIME_H_
+class Time
+{
+private:
+	int hours;
+	int minutes;
+public:
+	Time();
+	Time(int h,int m=0);
+	void addMin(int m);
+	void addHr(int h);
+	void Reset(int h=0, int m=0);
+	Time sum(const Time& t)const;
+	void Show();
+};
+#endif
